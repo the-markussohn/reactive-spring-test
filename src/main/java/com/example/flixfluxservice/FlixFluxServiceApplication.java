5e4fb7bb692c03795834820e997b164932aa92d4
the-markussohn <markus.justinas@gmail.com>
2018-07-08 23:02:23 +0300

    different approach

--- src/main/java/com/example/flixfluxservice/FlixFluxServiceApplication.java
+++ src/main/java/com/example/flixfluxservice/FlixFluxServiceApplication.java
@@ -6,16 +6,24 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
@@ -25,11 +33,55 @@ import java.util.Date;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
 @SpringBootApplication
 public class FlixFluxServiceApplication {
 
+    @Component
+    public static class MovieHandler {
+
+        private final MovieService movieService;
+
+        public MovieHandler(MovieService movieService) {
+            this.movieService = movieService;
+        }
+
+        public Mono<ServerResponse> byId(ServerRequest serverRequest) {
+            String movieId = serverRequest.pathVariable("movieId");
+            return ServerResponse.ok().body(this.movieService.byId(movieId), Movie.class);
+        }
+        public Mono<ServerResponse> all(ServerRequest serverRequest) {
+            return ServerResponse.ok().body(this.movieService.all(), Movie.class);
+        }
+        public Mono<ServerResponse> events(ServerRequest serverRequest) {
+            String movieId = serverRequest.pathVariable("movieId");
+            return ServerResponse.ok()
+                    .contentType(MediaType.TEXT_EVENT_STREAM)
+                    .body(this.movieService.events(movieId), MovieEvent.class);
+        }
+    }
+
+
+    @Bean
+    RouterFunction<?> route(MovieHandler movieHandler) {
+        return RouterFunctions.route(GET("/movies"), movieHandler::all)
+                .andRoute(GET("/movies/{movieId}"), movieHandler::byId)
+                .andRoute(GET("/movies/{movieId}/events"), movieHandler::events);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FlixFluxServiceApplication.class, args);
+    }
+}
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfiguration {
+    @Bean
+    MapReactiveUserDetailsService userDetailsService() {
+        UserDetails markussohn = User.withDefaultPasswordEncoder().username("markussohn").roles("USER").password("password").build();
+        return new MapReactiveUserDetailsService(markussohn);
     }
 }
 
@@ -57,6 +109,7 @@ class MovieCLR implements CommandLineRunner {
     }
 }
 
+/*
 @RestController
 @RequestMapping("/movies")
 class MovieRestController {
@@ -82,6 +135,7 @@ class MovieRestController {
         return movieService.events(movieId);
     }
 }
+*/
 
 @Service
 class MovieService {
